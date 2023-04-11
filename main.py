e60d42d4393e87674deda3a8fc05909963319329
Nicholas <nicholcosta@hotmail.com>
2023-04-11 17:10:10 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from pilot import getodd
+from betanoScrap import getodd
+import copy
 
-dados = getodd('https://br.betano.com/sport/basquete/eua/nba/17106/')
+dados_betano = copy.deepcopy(getodd('https://br.betano.com/sport/basquete/eua/nba/17106/'))
 
-print(dados['Miami Heat - Atlanta Hawks']['Vencedor SO'])
+print(dados_betano['Miami Heat - Atlanta Hawks']['Vencedor SO'])
 
 odd1 = 1
 odd2 = 3
